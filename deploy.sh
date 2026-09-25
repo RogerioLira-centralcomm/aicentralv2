a4119b461f66dad596868dec99b45d10082e69d7
apolocentralc <apolo@centralcomm.media>
2026-09-24 23:54:34 -0300

    Apply Cadu shared working memory migration during deploy

--- deploy.sh
+++ deploy.sh
@@ -327,6 +327,7 @@ if [ "$RUN_MIGRATIONS" = "1" ]; then
 "$VENV_PYTHON" migrations/run_sql_migration.py add_cadu_mcp_operations.sql
 "$VENV_PYTHON" migrations/run_add_cadu_mcp_contexts.py
 "$VENV_PYTHON" migrations/run_add_cadu_user_memory.py
+"$VENV_PYTHON" migrations/run_add_cadu_working_memory.py
 "$VENV_PYTHON" migrations/run_sql_migration.py add_cadu_conversation_memory.sql
 "$VENV_PYTHON" migrations/run_add_cadu_tool_token_ledger.py
 "$VENV_PYTHON" migrations/run_sql_migration.py add_cadu_credit_requests.sql
